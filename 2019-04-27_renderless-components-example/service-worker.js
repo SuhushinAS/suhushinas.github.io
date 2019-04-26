351dbe707d1cb4540b9a7ce86e886a079a8bad8d
SuhushinAS <a.suhushin@dev.userstory.ru>
2019-04-26 08:31:54 +0700

    fix

--- 2019-04-27_renderless-components-example/service-worker.js
+++ 2019-04-27_renderless-components-example/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.aa48e476a6e987abecdd53ec81a3a64e.js"
+  "./precache-manifest.6aa236da9433bbc76e0c877524547bd0.js"
 );
 
 workbox.clientsClaim();
